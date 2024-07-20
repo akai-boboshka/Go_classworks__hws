e72ae3528c2b46a8eb97bec1f0677a5f9f5ac9f7
akai-boboshka <ialisherzoda@gmail.com>
2024-07-20 22:27:08 +0500

    Added new text file

---
+++ lesson7.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func sayHello() {
+	fmt.Println("Hello Ibragim!")
+	fmt.Println("Yes, I did it!")
+}
--- main.go
+++ main.go
@@ -333,20 +333,19 @@ func checkNum(num int) bool {
 
 package main
 
-import "fmt"
-
 func main() {
 	// var ptr *int
 	// fmt.Println(ptr)
-	var math, physics, chemistry int
-	mathPtr := &math
-	physicsPtr := &physics
-	chemistryPtr := &chemistry
+	// var math, physics, chemistry int
+	// mathPtr := &math
+	// physicsPtr := &physics
+	// chemistryPtr := &chemistry
 
-	addGrade(mathPtr, 89)
-	addGrade(physicsPtr, 20)
-	addGrade(chemistryPtr, 98)
-	fmt.Printf("%.2f\n", printAverageGrade(mathPtr, physicsPtr, chemistryPtr))
+	// addGrade(mathPtr, 89)
+	// addGrade(physicsPtr, 20)
+	// addGrade(chemistryPtr, 98)
+	// fmt.Printf("%.2f\n", printAverageGrade(mathPtr, physicsPtr, chemistryPtr))
+	sayHello()
 }
 
 // #1
@@ -369,35 +368,35 @@ func main() {
 // }
 
 // Задача уровня мидл
-func vote(candidatePtr *string, votesPtr *int) {
-	*votesPtr++
-}
+// func vote(candidatePtr *string, votesPtr *int) {
+// 	*votesPtr++
+// }
 
-func winner(candidate1VotesPtr *int, candidate2VotesPtr *int) string {
-	if *candidate1VotesPtr > *candidate2VotesPtr {
-		return "Кандидат 1"
-	} else if *candidate2VotesPtr > *candidate1VotesPtr {
-		return "Кандидат 2"
-	} else {
-		return "Ничья"
-	}
-}
+// func winner(candidate1VotesPtr *int, candidate2VotesPtr *int) string {
+// 	if *candidate1VotesPtr > *candidate2VotesPtr {
+// 		return "Кандидат 1"
+// 	} else if *candidate2VotesPtr > *candidate1VotesPtr {
+// 		return "Кандидат 2"
+// 	} else {
+// 		return "Ничья"
+// 	}
+// }
 
-//
-func addExpense(totalPtr *float64, amount float64) {
-	*totalPtr += amount
-}
+// //
+// func addExpense(totalPtr *float64, amount float64) {
+// 	*totalPtr += amount
+// }
 
-func printTotalExpense(totalPtr *float64) {
-	fmt.Println("Общая сумма расходов: %.2f\n", *totalPtr)
-}
+// func printTotalExpense(totalPtr *float64) {
+// 	fmt.Println("Общая сумма расходов: %.2f\n", *totalPtr)
+// }
 
-//
-func addGrade(mathPtr *int, grade int) {
-	*mathPtr = grade
-}
+// //
+// func addGrade(mathPtr *int, grade int) {
+// 	*mathPtr = grade
+// }
 
-func printAverageGrade(mathPtr *int, physicsPtr *int, chemistryPtr *int) float {
-	sum := *mathPtr + *physicsPtr + *chemistryPtr
-	return float64(sum) / 3.0
-}
+// func printAverageGrade(mathPtr *int, physicsPtr *int, chemistryPtr *int) float {
+// 	sum := *mathPtr + *physicsPtr + *chemistryPtr
+// 	return float64(sum) / 3.0
+// }
